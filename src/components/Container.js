d99c2189fa6ad8a727d04bbb0f701c9101cdb412
Vukosavljev <vukosavljevs@gmail.com>
2020-04-23 20:47:45 +0200

    install react router.

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Header from './header/Header'
-import  Card from './card/Card'
+import React from "react";
+import Header from "./header/Header";
+import Card from "./card/Card";
 
 export default function container() {
-    return (
-        <div>
-            <Header />
-            <Card />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Card isNew={true} />
+    </div>
+  );
 }
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,27 +1,28 @@
-import React from 'react'
-import './card.scss'
-import discovercard from '../../assets/img/discovercard.png'
-import chip from '../../assets/img/chip.png'
+import React from "react";
+import "./card.scss";
+import discovercard from "../../assets/img/discovercard.png";
+import chip from "../../assets/img/chip.png";
 
-export default function card() {
-    return (
-        <div className="card-container"  >
-            <div className="card-logo" >
-                <img className="logo-img" src={discovercard} alt="card-logo"/>
-            </div>
+export default function card({ isNew }) {
+  const content = isNew ? (
+    <>
+      <div className="card-logo">
+        <img className="logo-img" src={discovercard} alt="card-logo" />
+      </div>
+      <div className="card-chip">
+        <img className="chip-img" src={chip} alt="chip" />
+      </div>
+      <div className="card-number">123456789</div>
+      <div className="card-name-and-date">
+        <div>User Name</div>
+        <div>02/22</div>
+      </div>{" "}
+    </>
+  ) : (
+    <div className="add-card">
+      <div className="add-sign">+</div>
+    </div>
+  );
 
-            <div className="card-chip">
-                <img className="chip-img" src={chip} alt="chip"/>
-            </div>
-
-            <div className="card-number" >
-                123456789
-            </div>
-
-            <div className="card-name-and-date">
-                <div>User Name</div>
-                <div>02/22</div>
-            </div>
-        </div>
-    )
+  return <div className="card-container">{content}</div>;
 }
